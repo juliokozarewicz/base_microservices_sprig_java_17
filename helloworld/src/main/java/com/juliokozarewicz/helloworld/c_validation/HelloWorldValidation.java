5baa371066bb4d77854680aaf882a61d27f60efe
Julio Kozarewicz <julioerk@outlook.com>
2025-02-05 11:34:24 -0300

    FEATURE: Validation inputs development.

--- helloworld/src/main/java/com/juliokozarewicz/helloworld/c_validation/HelloWorldValidation.java
+++ helloworld/src/main/java/com/juliokozarewicz/helloworld/c_validation/HelloWorldValidation.java
@@ -2,18 +2,7 @@ package com.juliokozarewicz.helloworld.c_validation;
 
 import jakarta.validation.constraints.Size;
 
-public class HelloWorldValidation {
-
-    @Size(min=1, max=100, message="{many_characters}")
-    private String message;
-
-    // Getters e Setters
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-}
+public record HelloWorldValidation(
+        @Size(min=1, max=100, message="{many_characters}")
+        String message
+) {}
