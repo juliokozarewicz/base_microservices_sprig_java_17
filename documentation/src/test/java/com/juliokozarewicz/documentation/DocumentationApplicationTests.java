5ad1f358e816908467981786032628c265bc8c9d
Ed <julioerk@outlook.com>
2025-02-14 18:47:54 -0300

    MERGE: Final API test.

---
+++ documentation/src/test/java/com/juliokozarewicz/documentation/DocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliokozarewicz.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
